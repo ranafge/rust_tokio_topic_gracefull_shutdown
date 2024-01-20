952e7d93a3a79b38420c19f9e3fbadb0885d1a5b
ranafge <ranafge@gmail.com>
2024-01-21 04:05:30 +0600

    for shuddown tasktracker added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::thread::sleep;
+use std::time::Duration;
+
 use tokio::signal;
 #[tokio::main]
 async fn main() {
@@ -9,4 +12,41 @@ async fn main() {
             eprintln!("Unable to listen for shutdown signal: {}", e);
         }
     }
+}
+// if there is a mutiple shutdown condition use mpsc channel to send the shutdown signal to one place.
+// can use select on ctrl_c
+
+// Example
+
+use tokio::sync::mpsc;
+
+// #[tokio::main]
+// async fn main() {
+//     let (shudown_send, mut shutdown_recv) = mpsc::unbounded_channel();
+//     // other task 
+
+//     tokio::select! {
+//         _= signal::ctrl_c() => {},
+//         _= shutdown_recv.recv() => {}
+//     }
+// }
+
+use tokio_util::sync::CancellationToken;
+
+#[tokio::main] 
+
+async fn main() {
+    let tracker = TaskTracker::new();
+
+    for i in 0..10 {
+        tracker.spawn(some_operation(i));
+    }
+    tracker.close();
+    tracker.wait().await;
+    println!("tHIS IS PRINTED AFTER ALL OF THE TASKS");
+}
+
+async fn some_operation(i: u64) {
+    sleep(Duration::from_millis(10));
+    println!("Task {} shutting down.", i);
 }
